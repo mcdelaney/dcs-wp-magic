e43f7438432394f9d4cb899415d30b0b9a1548b2
mcdelaney <mcdelaney@gmail.com>
2019-09-16 13:31:05 -0400

    - only start programs if not running - add joystick gremlin

--- start_dcs_programs.bat
+++ start_dcs_programs.bat
@@ -1,7 +1,10 @@
 @echo off
-CD "C:\Program Files (x86)\DCS-SimpleRadio-Standalone\"
-start "" "SR-ClientRadio.exe"
-start "" "C:\Program Files (x86)\Steam\steamapps\common\VoiceAttack\VoiceAttack.exe"
+start C:\cygwin64\bin\bash --login -c "cd C:/Users/mcdel/dcs-wb-magic/; python run_servers.py"
 
-CD "C:\Users\mcdel\dcs-wb-magic"
-start "servers" "C:\Users\mcdel\Anaconda3\python.exe" "C:/Users/mcdel/dcs-wb-magic/run_servers.py"
+CD "C:\Program Files (x86)\DCS-SimpleRadio-Standalone\"
+
+tasklist /nh /fi "imagename eq SR-ClientRadio.exe" | find /i "SR-ClientRadio.exe" > nul || (start "" "SR-ClientRadio.exe")
+
+tasklist /nh /fi "imagename eq VoiceAttack.exe" | find /i "VoiceAttack.exe" > nul || (start "" "C:\Program Files (x86)\Steam\steamapps\common\VoiceAttack\VoiceAttack.exe")
+
+tasklist /nh /fi "imagename eq joystick_gremlin.exe" | find /i "joystick_gremlin.exe" > nul || (start "" "C:\Users\mcdel\Desktop\Joystick Gremlin.lnk")
