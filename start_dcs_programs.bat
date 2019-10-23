007c16818cfc8b779e8341855b6cd9df6dc612b6
mcdelaney <mcdelaney@gmail.com>
2019-10-23 12:07:24 -0400

    Fix joystick gremlin statup path

--- start_dcs_programs.bat
+++ start_dcs_programs.bat
@@ -5,6 +5,7 @@ tasklist /nh /fi "imagename eq SR-ClientRadio.exe" | find /i "SR-ClientRadio.exe
 
 tasklist /nh /fi "imagename eq VoiceAttack.exe" | find /i "VoiceAttack.exe" > nul || (start "" "C:\Program Files (x86)\Steam\steamapps\common\VoiceAttack\VoiceAttack.exe")
 
+CD "C:\Program Files (x86)\H2ik\Joystick Gremlin\"
 tasklist /nh /fi "imagename eq joystick_gremlin.exe" | find /i "joystick_gremlin.exe" > nul || (start "" "C:\Program Files (x86)\H2ik\Joystick Gremlin\joystick_gremlin.exe")
 
 CD "C:\Users\mcdel\dcs-wp-magic\"
