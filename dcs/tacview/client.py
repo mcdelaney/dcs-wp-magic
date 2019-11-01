742c0135989fd8912a95069e2c5d140c9d1f0cca
Matt Delaney <mattdelaney@google.com>
2019-11-01 19:17:41 -0400

    update client

--- dcs/tacview/client.py
+++ dcs/tacview/client.py
@@ -153,7 +153,7 @@ def process_line(obj_dict, pubsub=None):
 
         event.save()
         if pubsub:
-            pubsub_rec = model_to_dict(event.get())
+            pubsub_rec = model_to_dict(event)
             obj_id = pubsub_rec.pop('object')
             pubsub_rec['object'] = obj_id['id']
             pubsub_rec = json.dumps(pubsub_rec,
