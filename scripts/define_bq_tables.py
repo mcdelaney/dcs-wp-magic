dca93ba580b8928da168bade80232d89be157bcd
Matt Delaney <mattdelaney@google.com>
2019-11-01 17:38:14 -0400

    Add scripts to define bigquery tables

---
+++ scripts/define_bq_tables.py
@@ -0,0 +1,66 @@
+from google.cloud import bigquery
+
+client = bigquery.Client()
+
+
+events_schema = [
+    bigquery.SchemaField("id", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("session_id", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("object", "STRING", mode="NULLABLE"),
+    bigquery.SchemaField("alive", "INTEGER", mode="NULLABLE"),
+    bigquery.SchemaField("last_seen", "timestamp", mode="NULLABLE"),
+    bigquery.SchemaField("lat", "FLOAT", mode="NULLABLE"),
+    bigquery.SchemaField("long", "FLOAT", mode="NULLABLE"),
+    bigquery.SchemaField("alt", "float", mode="NULLABLE"),
+    bigquery.SchemaField("roll", "float", mode="NULLABLE"),
+    bigquery.SchemaField("pitch", "float", mode="NULLABLE"),
+    bigquery.SchemaField("yaw", "float", mode="NULLABLE"),
+    bigquery.SchemaField("u_coord", "float", mode="NULLABLE"),
+    bigquery.SchemaField("v_coord", "float", mode="NULLABLE"),
+    bigquery.SchemaField("heading", "float", mode="NULLABLE"),
+    bigquery.SchemaField("dist_m", "float", mode="NULLABLE"),
+    bigquery.SchemaField("velocity_ms", "float", mode="NULLABLE"),
+    bigquery.SchemaField("secs_from_last", "float", mode="NULLABLE"),
+    bigquery.SchemaField("update_num", "INTEGER", mode="NULLABLE")
+]
+
+events_tbl_id = "dcs-analytics-257714.tacview.events"
+table = bigquery.Table(events_tbl_id, schema=events_schema)
+table = client.create_table(table)
+
+
+objects_schema = [
+    bigquery.SchemaField("id", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("session_id", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("object", "STRING", mode="NULLABLE"),
+    bigquery.SchemaField("color", "STRING", mode="NULLABLE"),
+    bigquery.SchemaField("country", "STRING", mode="NULLABLE"),
+    bigquery.SchemaField("grp", "STRING", mode="NULLABLE"),
+    bigquery.SchemaField("pilot", "STRING", mode="NULLABLE"),
+    bigquery.SchemaField("platform", "STRING", mode="NULLABLE"),
+    bigquery.SchemaField("type", "STRING", mode="NULLABLE"),
+    bigquery.SchemaField("coalition", "STRING", mode="NULLABLE"),
+    bigquery.SchemaField("first_seen", "timestamp", mode="NULLABLE"),
+    bigquery.SchemaField("alive", "INTEGER", mode="NULLABLE"),
+    bigquery.SchemaField("last_seen", "timestamp", mode="NULLABLE"),
+    bigquery.SchemaField("lat", "FLOAT", mode="NULLABLE"),
+    bigquery.SchemaField("long", "FLOAT", mode="NULLABLE"),
+    bigquery.SchemaField("alt", "float", mode="NULLABLE"),
+    bigquery.SchemaField("roll", "float", mode="NULLABLE"),
+    bigquery.SchemaField("pitch", "float", mode="NULLABLE"),
+    bigquery.SchemaField("yaw", "float", mode="NULLABLE"),
+    bigquery.SchemaField("u_coord", "float", mode="NULLABLE"),
+    bigquery.SchemaField("v_coord", "float", mode="NULLABLE"),
+    bigquery.SchemaField("heading", "float", mode="NULLABLE"),
+    bigquery.SchemaField("dist_m", "float", mode="NULLABLE"),
+    bigquery.SchemaField("velocity_ms", "float", mode="NULLABLE"),
+    bigquery.SchemaField("secs_from_last", "float", mode="NULLABLE"),
+    bigquery.SchemaField("updates", "INTEGER", mode="NULLABLE"),
+    bigquery.SchemaField("parent", "STRING", mode="NULLABLE")
+]
+
+
+objects_tbl_id = "dcs-analytics-257714.tacview.objects"
+
+table = bigquery.Table(objects_tbl_id, schema=objects_schema)
+table = client.create_table(table)
