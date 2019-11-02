e7575835afc2b42514c3a60c5a375a08f55d1473
Matt Delaney <mattdelaney@google.com>
2019-11-02 13:59:51 -0400

    clean up bigquery script

--- scripts/define_bq_tables.py
+++ scripts/define_bq_tables.py
@@ -1,102 +1,108 @@
-from google.cloud import bigquery
-
-client = bigquery.Client()
-
-
-events_schema = [
-    bigquery.SchemaField("id", "STRING", mode="REQUIRED"),
-    bigquery.SchemaField("session_id", "STRING", mode="REQUIRED"),
-    bigquery.SchemaField("object", "STRING", mode="NULLABLE"),
-    bigquery.SchemaField("alive", "INTEGER", mode="NULLABLE"),
-    bigquery.SchemaField("last_seen", "TIMESTAMP", mode="NULLABLE"),
-    bigquery.SchemaField("lat", "FLOAT", mode="NULLABLE"),
-    bigquery.SchemaField("long", "FLOAT", mode="NULLABLE"),
-    bigquery.SchemaField("alt", "float", mode="NULLABLE"),
-    bigquery.SchemaField("roll", "float", mode="NULLABLE"),
-    bigquery.SchemaField("pitch", "float", mode="NULLABLE"),
-    bigquery.SchemaField("yaw", "float", mode="NULLABLE"),
-    bigquery.SchemaField("u_coord", "float", mode="NULLABLE"),
-    bigquery.SchemaField("v_coord", "float", mode="NULLABLE"),
-    bigquery.SchemaField("heading", "float", mode="NULLABLE"),
-    bigquery.SchemaField("dist_m", "float", mode="NULLABLE"),
-    bigquery.SchemaField("velocity_ms", "float", mode="NULLABLE"),
-    bigquery.SchemaField("secs_from_last", "float", mode="NULLABLE"),
-    bigquery.SchemaField("update_num", "INTEGER", mode="NULLABLE")
-]
-try:
-    events_tbl_id = "dcs-analytics-257714.tacview.events"
-    table = bigquery.Table(events_tbl_id, schema=events_schema)
-    table.time_partitioning = bigquery.TimePartitioning(
-        type_=bigquery.TimePartitioningType.DAY,
-        field="last_seen",  # name of column to use for partitioning
-        expiration_ms=None,
-    )
-
-    table = client.create_table(table)
-except Exception as err:
-    print(err)
-    print("error creating events!")
+"""Script to create bigquery tables for event stream."""
+from google.cloud import bigquery # pylint: disable=no-name-in-module
 
 
-objects_schema = [
-    bigquery.SchemaField("id", "STRING", mode="REQUIRED"),
-    bigquery.SchemaField("session_id", "STRING", mode="REQUIRED"),
-    bigquery.SchemaField("name", "STRING", mode="NULLABLE"),
-    bigquery.SchemaField("color", "STRING", mode="NULLABLE"),
-    bigquery.SchemaField("country", "STRING", mode="NULLABLE"),
-    bigquery.SchemaField("grp", "STRING", mode="NULLABLE"),
-    bigquery.SchemaField("pilot", "STRING", mode="NULLABLE"),
-    bigquery.SchemaField("platform", "STRING", mode="NULLABLE"),
-    bigquery.SchemaField("type", "STRING", mode="NULLABLE"),
-    bigquery.SchemaField("coalition", "STRING", mode="NULLABLE"),
-    bigquery.SchemaField("first_seen", "TIMESTAMP", mode="NULLABLE"),
-    bigquery.SchemaField("alive", "INTEGER", mode="NULLABLE"),
-    bigquery.SchemaField("last_seen", "TIMESTAMP", mode="NULLABLE"),
-    bigquery.SchemaField("lat", "FLOAT", mode="NULLABLE"),
-    bigquery.SchemaField("long", "FLOAT", mode="NULLABLE"),
-    bigquery.SchemaField("alt", "float", mode="NULLABLE"),
-    bigquery.SchemaField("roll", "float", mode="NULLABLE"),
-    bigquery.SchemaField("pitch", "float", mode="NULLABLE"),
-    bigquery.SchemaField("yaw", "float", mode="NULLABLE"),
-    bigquery.SchemaField("u_coord", "float", mode="NULLABLE"),
-    bigquery.SchemaField("v_coord", "float", mode="NULLABLE"),
-    bigquery.SchemaField("heading", "float", mode="NULLABLE"),
-    bigquery.SchemaField("dist_m", "float", mode="NULLABLE"),
-    bigquery.SchemaField("velocity_ms", "float", mode="NULLABLE"),
-    bigquery.SchemaField("secs_from_last", "float", mode="NULLABLE"),
-    bigquery.SchemaField("updates", "INTEGER", mode="NULLABLE"),
-    bigquery.SchemaField("parent", "STRING", mode="NULLABLE")
-]
+def main():
+    client = bigquery.Client()
 
-try:
-    objects_tbl_id = "dcs-analytics-257714.tacview.objects"
-    table = bigquery.Table(objects_tbl_id, schema=objects_schema)
-    table.time_partitioning = bigquery.TimePartitioning(
-        type_=bigquery.TimePartitioningType.DAY,
-        field="first_seen",  # name of column to use for partitioning
-        expiration_ms=None,
-    )
+    events_schema = [
+        bigquery.SchemaField("id", "STRING", mode="REQUIRED"),
+        bigquery.SchemaField("session_id", "STRING", mode="REQUIRED"),
+        bigquery.SchemaField("object", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("alive", "INTEGER", mode="NULLABLE"),
+        bigquery.SchemaField("last_seen", "TIMESTAMP", mode="NULLABLE"),
+        bigquery.SchemaField("lat", "FLOAT", mode="NULLABLE"),
+        bigquery.SchemaField("long", "FLOAT", mode="NULLABLE"),
+        bigquery.SchemaField("alt", "float", mode="NULLABLE"),
+        bigquery.SchemaField("roll", "float", mode="NULLABLE"),
+        bigquery.SchemaField("pitch", "float", mode="NULLABLE"),
+        bigquery.SchemaField("yaw", "float", mode="NULLABLE"),
+        bigquery.SchemaField("u_coord", "float", mode="NULLABLE"),
+        bigquery.SchemaField("v_coord", "float", mode="NULLABLE"),
+        bigquery.SchemaField("heading", "float", mode="NULLABLE"),
+        bigquery.SchemaField("dist_m", "float", mode="NULLABLE"),
+        bigquery.SchemaField("velocity_ms", "float", mode="NULLABLE"),
+        bigquery.SchemaField("secs_from_last", "float", mode="NULLABLE"),
+        bigquery.SchemaField("update_num", "INTEGER", mode="NULLABLE")
+    ]
+    try:
+        events_tbl_id = "dcs-analytics-257714.tacview.events"
+        table = bigquery.Table(events_tbl_id, schema=events_schema)
+        table.time_partitioning = bigquery.TimePartitioning(
+            type_=bigquery.TimePartitioningType.DAY,
+            field="last_seen",  # name of column to use for partitioning
+            expiration_ms=None,
+        )
 
-    table = client.create_table(table)
-except Exception as err:
-    print(err)
-    print("error creating objects!")
+        table = client.create_table(table)
+    except Exception as err:
+        print(err)
+        print("error creating events!")
 
 
-sessions_schema = [
-    bigquery.SchemaField("session_id", "STRING", mode="REQUIRED"),
-    bigquery.SchemaField("title", "STRING", mode="REQUIRED"),
-    bigquery.SchemaField("author", "STRING", mode="REQUIRED"),
-    bigquery.SchemaField("datasource", "STRING", mode="REQUIRED"),
-    bigquery.SchemaField("start_time", "TIMESTAMP", mode="REQUIRED"),
-    bigquery.SchemaField("lat", "FLOAT", mode="REQUIRED"),
-    bigquery.SchemaField("long", "FLOAT", mode="REQUIRED"),
+    objects_schema = [
+        bigquery.SchemaField("id", "STRING", mode="REQUIRED"),
+        bigquery.SchemaField("session_id", "STRING", mode="REQUIRED"),
+        bigquery.SchemaField("name", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("color", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("country", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("grp", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("pilot", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("platform", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("type", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("coalition", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("first_seen", "TIMESTAMP", mode="NULLABLE"),
+        bigquery.SchemaField("alive", "INTEGER", mode="NULLABLE"),
+        bigquery.SchemaField("last_seen", "TIMESTAMP", mode="NULLABLE"),
+        bigquery.SchemaField("lat", "FLOAT", mode="NULLABLE"),
+        bigquery.SchemaField("long", "FLOAT", mode="NULLABLE"),
+        bigquery.SchemaField("alt", "float", mode="NULLABLE"),
+        bigquery.SchemaField("roll", "float", mode="NULLABLE"),
+        bigquery.SchemaField("pitch", "float", mode="NULLABLE"),
+        bigquery.SchemaField("yaw", "float", mode="NULLABLE"),
+        bigquery.SchemaField("u_coord", "float", mode="NULLABLE"),
+        bigquery.SchemaField("v_coord", "float", mode="NULLABLE"),
+        bigquery.SchemaField("heading", "float", mode="NULLABLE"),
+        bigquery.SchemaField("dist_m", "float", mode="NULLABLE"),
+        bigquery.SchemaField("velocity_ms", "float", mode="NULLABLE"),
+        bigquery.SchemaField("secs_from_last", "float", mode="NULLABLE"),
+        bigquery.SchemaField("updates", "INTEGER", mode="NULLABLE"),
+        bigquery.SchemaField("parent", "STRING", mode="NULLABLE")
+    ]
 
-]
-try:
-    sessions_tbl_id = "dcs-analytics-257714.tacview.sessions"
-    table = bigquery.Table(sessions_tbl_id, schema=sessions_schema)
-    table = client.create_table(table)
-except Exception as err:
-    print(err)
-    print("error creating sessions!")
+    try:
+        objects_tbl_id = "dcs-analytics-257714.tacview.objects"
+        table = bigquery.Table(objects_tbl_id, schema=objects_schema)
+        table.time_partitioning = bigquery.TimePartitioning(
+            type_=bigquery.TimePartitioningType.DAY,
+            field="first_seen",  # name of column to use for partitioning
+            expiration_ms=None,
+        )
+
+        table = client.create_table(table)
+    except Exception as err:
+        print(err)
+        print("error creating objects!")
+
+
+    sessions_schema = [
+        bigquery.SchemaField("session_id", "STRING", mode="REQUIRED"),
+        bigquery.SchemaField("title", "STRING", mode="REQUIRED"),
+        bigquery.SchemaField("author", "STRING", mode="REQUIRED"),
+        bigquery.SchemaField("datasource", "STRING", mode="REQUIRED"),
+        bigquery.SchemaField("start_time", "TIMESTAMP", mode="REQUIRED"),
+        bigquery.SchemaField("lat", "FLOAT", mode="REQUIRED"),
+        bigquery.SchemaField("long", "FLOAT", mode="REQUIRED"),
+
+    ]
+    try:
+        sessions_tbl_id = "dcs-analytics-257714.tacview.sessions"
+        table = bigquery.Table(sessions_tbl_id, schema=sessions_schema)
+        table = client.create_table(table)
+    except Exception as err:
+        print(err)
+        print("error creating sessions!")
+
+
+if __name__ == "__main__":
+    main()
