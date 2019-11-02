d2a09ed34e1b55fd81b4d54b8637307eb248e48e
Matt Delaney <mattdelaney@google.com>
2019-11-02 14:50:16 -0400

    ensure distinct objects and sessions on join

--- scripts/tacview_comb.sql
+++ scripts/tacview_comb.sql
@@ -1,10 +1,11 @@
 CREATE OR REPLACE VIEW `dcs-analytics-257714`.tacview.comb AS (
   SELECT * FROM `dcs-analytics-257714`.tacview.events
-  INNER JOIN (SELECT title, datasource, session_id
+  INNER JOIN (SELECT DISTINCT session_id, title, datasource
               FROM `dcs-analytics-257714`.tacview.sessions) sessions
   USING (session_id)
-  INNER JOIN (SELECT id as object, session_id, name,
-                color, country, grp, pilot, type, coalition, parent
+  INNER JOIN (SELECT DISTINCT id as object, session_id, name,
+                color, country, grp, pilot, type, first_seen,
+                coalition, parent
              FROM `dcs-analytics-257714`.tacview.objects)
   USING (object, session_id)
 )
