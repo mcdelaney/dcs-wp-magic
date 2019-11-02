d4b5f752d667d7406f64e2ce423fd972a0ca70b0
Matt Delaney <mattdelaney@google.com>
2019-11-02 15:03:31 -0400

    update query

--- scripts/tacview_comb.sql
+++ scripts/tacview_comb.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE VIEW `dcs-analytics-257714`.tacview.comb AS (
-  SELECT * FROM `dcs-analytics-257714`.tacview.events
+  SELECT *, ST_GEOGPOINT(long, lat) as geo_pt
+  FROM `dcs-analytics-257714`.tacview.events
   INNER JOIN (SELECT DISTINCT session_id, title, datasource
               FROM `dcs-analytics-257714`.tacview.sessions) sessions
   USING (session_id)
