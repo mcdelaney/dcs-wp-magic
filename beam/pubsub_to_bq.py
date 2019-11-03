11a7d6b783079a99d9adb6c788ff7b271fadd92f
Matt Delaney <mattdelaney@google.com>
2019-11-03 12:04:42 -0500

    updates

--- beam/pubsub_to_bq.py
+++ beam/pubsub_to_bq.py
@@ -1,3 +1,4 @@
+"""Experimental python pubsub pipeline."""
 import argparse
 import datetime
 import json
@@ -13,10 +14,8 @@ class GroupWindowsIntoBatches(beam.PTransform):
     time and outputs a list of dictionaries, where each contains one message
     and its publish timestamp.
     """
-
     def __init__(self, window_size):
-        # Convert minutes into seconds.
-        self.window_size = int(window_size * 60)
+        self.window_size = window_size
 
     def expand(self, pcoll):
         return (pcoll
@@ -30,9 +29,10 @@ class GroupWindowsIntoBatches(beam.PTransform):
                 # for this. If the windowed elements do not fit into memory,
                 # please consider using `beam.util.BatchElements`.
                 # https://beam.apache.org/releases/pydoc/current/apache_beam.transforms.util.html#apache_beam.transforms.util.BatchElements
-                | 'Add Session Key' >> beam.Map(lambda elem: ('session_id', elem))
+                # | 'Add Session Key' >> beam.Map(lambda elem: ('session_id', elem))
+                | 'Add Dummy Key' >> beam.Map(lambda elem: (None, elem))
                 | 'Groupby' >> beam.GroupByKey()
-                # | 'Abandon Dummy Key' >> beam.MapTuple(lambda _, val: val))
+                | 'Abandon Dummy Key' >> beam.MapTuple(lambda _, val: val))
 
 
 class AddTimestamps(beam.DoFn):
@@ -43,14 +43,21 @@ class AddTimestamps(beam.DoFn):
         defaults to the publish timestamp returned by the Pub/Sub server. It
         is bound to each element by Beam at runtime.
         """
-        msg = element.decode('utf-8')
-        yield {
-            'message_body': msg,
-            'publish_time': datetime.datetime.strptime(msg['last_seen'],
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
-            # 'publish_time': datetime.datetime.utcfromtimestamp(
-            #     float(publish_time)).strftime("%Y-%m-%d %H:%M:%S.%f"),
-        }
+        msg = json.loads(element.decode('utf-8'))
+        try:
+            tstamp = datetime.datetime.strptime(msg['last_seen'],
+                                                '%Y-%m-%dT%H:%M:%S.%f')
+        except ValueError:
+            tstamp = datetime.datetime.strptime(msg['last_seen'],
+                                                '%Y-%m-%dT%H:%M:%S')
+
+        msg['publish_time'] = float(tstamp)
+        yield msg
+
+        # yield {
+        #     'message_body': msg,
+        #     'publish_time': msg['last_seen']
+        # }
 
 
 class WriteBatchesToGCS(beam.DoFn):
@@ -89,15 +96,16 @@ if __name__ == '__main__': # noqa
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--input_topic', default="projects/dcs-analytics-257714/topics/events",
+        '--input_topic', default="projects/dcs-analytics-257714/topics/tacview_events",
         help='The PubSub topic to read from')
     parser.add_argument(
         '--window_size',
-        type=float,
-        default=10.0,
+        type=int,
+        default=10,
         help='Output file\'s window size in number of seconds.')
     parser.add_argument(
-        '--output_path', default='gs://dcs-analytics-dataflow/py_test/events'
+        '--output_path',
+        default='gs://dcs-analytics-dataflow/py_test/events',
         help='GCS Path of the output file including filename prefix.')
     known_args, pipeline_args = parser.parse_known_args()
 
