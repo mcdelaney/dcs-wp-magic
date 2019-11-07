7e6c651b53499071ff87bca3c6b0e5cbc5d78311
Matt Delaney <mattdelaney@google.com>
2019-11-07 10:17:41 -0500

    updates

--- dcs-java/src/main/java/com/javadcs/TacviewClient.java
+++ dcs-java/src/main/java/com/javadcs/TacviewClient.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Arrays;
 import java.util.List;
@@ -15,66 +14,10 @@ public class TacviewClient
 
     public static void main(String[] args) throws Exception
     {
-        // Thread serverThread = new Thread(new Server());
-        // serverThread.start();
         new Client().run();
-        // client.run();
         // Thread clientThread = new Thread(new Client());
         // clientThread.start();
-        // clientThread.join();
-        // serverThread.join();
     }
-
-    // private static class Server implements Runnable
-    // {
-    //     @Override
-    //     public void run()
-    //     {
-    //         ServerSocket serverSocket = null;
-    //         try
-    //         {
-    //             serverSocket = new ServerSocket(5555);
-    //
-    //             Socket clientSocket = null;
-    //             clientSocket = serverSocket.accept();
-    //             debug("Connected");
-    //
-    //             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-    //             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-    //
-    //             String textFromClient = null;
-    //             String textToClient = null;
-    //             textFromClient = in.readLine(); // read the text from client
-    //             debug("Read '" + textFromClient + "'");
-    //             if ("A".equals(textFromClient))
-    //             {
-    //                 textToClient = "1111";
-    //             }
-    //             else if ("B".equals(textFromClient))
-    //             {
-    //                 textToClient = "2222\r\n3333";
-    //             }
-    //
-    //             debug("Writing '" + textToClient + "'");
-    //             out.print(textToClient + "\r\n"); // send the response to client
-    //             out.flush();
-    //             out.close();
-    //             in.close();
-    //             clientSocket.close();
-    //             serverSocket.close();
-    //         }
-    //         catch (Exception e)
-    //         {
-    //             e.printStackTrace();
-    //         }
-    //
-    //     }
-    //
-    //     private static void debug(String msg)
-    //     {
-    //         System.out.println("Server: " + msg);
-    //     }
-    // }
 
     private static class Client implements Runnable
     {
@@ -86,8 +29,7 @@ public class TacviewClient
                                    "XtraLib.Stream.0",
                                    "Tacview.RealTimeTelemetry.0",
                                    "tacview_reader",
-                                   "0",
-                                   "\0"
+                                   "0"
               );
             String handshake = String.join("\n", handshake_params);
 
@@ -98,18 +40,19 @@ public class TacviewClient
             try
             {
                 socket = new Socket("127.0.0.1", 5555);
+                // socket = new Socket("147.135.8.169", 42674); # Gaw
                 out = new PrintWriter(socket.getOutputStream(), true);
                 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 debug("Connected");
 
                 // textToServer = read.readLine();
                 debug("Sending '" + handshake + "'");
-                out.print(handshake + "\r\n"); // send to server
+                out.print(handshake + "\0"); // send to server
                 out.flush();
 
-                String serverResponse = null;
-                while ((serverResponse = in.readLine()) != null)
-                    debug(serverResponse); // read from server and print it.
+                String obj = null;
+                while ((obj = in.readLine()) != null)
+                    debug(obj); // read from server and print it.
 
                 out.close();
                 in.close();
