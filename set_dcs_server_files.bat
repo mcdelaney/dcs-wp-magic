2ac5130aababe69898d0fff0444ff35b369037b6
mcdelaney <mcdelaney@gmail.com>
2020-01-14 09:35:49 -0500

    Add server update bat file

---
+++ set_dcs_server_files.bat
@@ -0,0 +1,7 @@
+@echo off
+
+xcopy C:\Users\mcdel\Dropbox\Missions "C:\Users\mcdel\Saved Games\DCS.openbeta_server\Missions" /y
+xcopy C:\Users\mcdel\Dropbox\MissionScripting  "C:\Users\mcdel\Saved Games\DCS.openbeta_server\MissionScripting" /y
+
+REM Ensure santatize lua is disabled for mission persistence
+copy C:\Users\mcdel\Dropbox\MissionScripting\MissionScripting.lua  "C:\Users\Program Files\Eagle Dynamics\DCS.openbeta_server\Scripts\MissionScripting.lua"
