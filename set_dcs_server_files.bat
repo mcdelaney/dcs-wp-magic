5d3f68cd42d028c8237bf4c0b1b0d1be14a7fb18
mcdelaney <mcdelaney@gmail.com>
2020-01-14 09:58:26 -0500

    updates

--- set_dcs_server_files.bat
+++ set_dcs_server_files.bat
@@ -5,3 +5,5 @@ xcopy C:\Users\mcdel\Dropbox\MissionScripting  "C:\Users\mcdel\Saved Games\DCS.o
 
 REM Ensure santatize lua is disabled for mission persistence
 copy C:\Users\mcdel\Dropbox\MissionScripting\MissionScripting.lua  "C:\Users\Program Files\Eagle Dynamics\DCS.openbeta_server\Scripts\MissionScripting.lua"
+
+copy C:\Users\mcdel\Dropbox\MissionScripting\slmod_config.lua  "C:\Users\Program Files\Eagle Dynamics\DCS.openbeta_server\Slmod\config.lua"
